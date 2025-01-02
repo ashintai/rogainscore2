6d8a31b89d6e842729834a2b7c4d7f6d4afd3d65
ashintai <shintai.akira@gmail.com>
2025-01-03 07:58:40 +0900

    css

--- resources/views/check_point.blade.php
+++ resources/views/check_point.blade.php
@@ -64,14 +64,14 @@
     <div style="display: flex">
         <div style="width: 45%">
             <p>取得写真</p>
-            <div style="width: 100%; height:auto;">
-                <img src="{{ $get_photo_url }}" alt="取得写真" >
+            <div >
+                <img src="{{ $get_photo_url }}" alt="取得写真" style="width: 100%; height:auto;">
             </div>
         </div>
         <div style="width: 45%">
             <p>正解写真</p>
-            <div style="width: 100%; height:auto;">
-                <img src="{{ $set_photo_url }}" alt="正解写真" >
+            <div >
+                <img src="{{ $set_photo_url }}" alt="正解写真" style="width: 100%; height:auto;">
             </div>
         </div>
     </div>
