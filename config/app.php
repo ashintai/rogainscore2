c291b09239e136ec1e3de1b8c62e972c71b0fb58
ashintai <shintai.akira@gmail.com>
2025-01-18 12:35:05 +0900

    Intervention

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Intervention\Image\ImageServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -183,7 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        // 'Image' => Intervention\Image\Facades\Image::class,
         ])->toArray(),
 
 ];
