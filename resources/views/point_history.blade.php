7ecdbed6690d81ba6a05815b2f05d267711d5b78
ashintai <shintai.akira@gmail.com>
2025-01-25 15:18:04 +0900

    return

--- resources/views/point_history.blade.php
+++ resources/views/point_history.blade.php
@@ -13,6 +13,11 @@
 <body>
 <header>
     <h5 style="color: blue;">モリコロロゲイニング　ポイント履歴一覧</h5>
+    <!-- 戻るボタン -->
+    <div class="ms-3">
+    <a href="{{ route('staff_main') }}" class="btn btn-primary">戻る</a>
+    </div>
+    
     <!-- ログアウトボタン -->
     <form action="{{ route('logout') }}" method="POST" >
         @csrf
