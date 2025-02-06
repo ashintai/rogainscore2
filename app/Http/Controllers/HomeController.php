8fa65e2952a8bcb0ff2f6babec843204974ff7a5
ashintai <shintai.akira@gmail.com>
2025-02-06 17:14:41 +0900

    debug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -955,13 +955,10 @@ public function getResults(Request $request)
         $result_str = "";
         $total_score = 0;
 
-        // foreach ($get_points as $get_point) {
+        foreach ($get_points as $get_point) {
         //     $result_str .= $get_point->point_no . ":" . $get_point->setPoint->point_name . "ー";
         //     $total_score += $get_point->setPoint->score;
-        // }
-        
-        // でバック
-        $result_str = $get_points->count();
+        }
         
         // 減点を加える
         $total_score -= $penalty;
