8e23b23e78ae07c403679c06e16fa1c8d694ce9f
ashintai <shintai.akira@gmail.com>
2025-04-26 15:46:09 +0900

    canvasDraw4

--- resources/views/test_canvas.blade.php
+++ resources/views/test_canvas.blade.php
@@ -15,6 +15,20 @@
 
 
 <body>
+
+<!-- 原文のまま -->
+<form action="" method="post" id="imageForm">
+        <img src="" id="preview" />
+        <!-- canvas要素の生成 -->
+        <canvas id="canvas"></canvas>
+        <!-- 画像ファイルの入力→onChangeでcanvasDraw()を実行 -->
+        <input type="file" id="imageSelect" onChange="canvasDraw();" />
+        <!-- ボタンクリックでimageUpload()を実行 -->
+        <input type="button" onClick="imageUpload();" value="アップロード" />
+    </form>
+
+
+<!-- 
     <form id="upload-form" action="{{ route('canvas_upload_test') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <img src="" id="preview" />
@@ -24,7 +38,7 @@
                 <input type="file" class="form-control" id="imageSelect"  onChange="canvasDraw();" name="image" accept="image/*" required >
             </div>
             <input type="button" onClick="" value="アップロード" />
-        </form>
+        </form> -->
 </body>
 
 
@@ -33,14 +47,25 @@
 
 </html>
 
-<script>
-    function canvasDraw() {
 
+<!-- 原文のまま -->
+<script>
+    // 画像が選択された時に呼び出される関数
+function canvasDraw() {
     // id=imageSelect の特性propatyのfileの1番目[0]を変数fileへ取得
     var file = $("#imageSelect").prop("files")[0];
- 
-    var fr = new FileReader();
-    fr.onload = function() {
+                
+    //画像ファイルかチェック
+    // file["type"]はファイルの種類を表すプロパティ
+    if (file["type"] != "image/jpeg" && file["type"] != "image/png" && file["type"] != "image/gif") {
+        alert("画像ファイルを選択してください");
+        $("#imageSelect").val(''); //選択したファイルをクリア
+
+    } else {
+        // FileReaderで　ローカルファイルをjavascriptで処理できるようになる
+        var fr = new FileReader();
+        // onloadはファイルが選択されてから処理する関数を定義
+        fr.onload = function() {
             //選択した画像を一旦imgタグに表示
             $("#preview").attr('src', fr.result);
                         
@@ -49,7 +74,7 @@
             image.src = $("#preview").attr('src');
                         
             //縦横比を維持した縮小サイズを取得
-            var w = 600;
+            var w = 800;
             var ratio = w / image.width;
             var h = image.height * ratio;
                         
@@ -62,5 +87,38 @@
         };
 
         fr.readAsDataURL(file);
+    }
+}
 
-</script>
+
+// <script>
+//     function canvasDraw() {
+
+//     // id=imageSelect の特性propatyのfileの1番目[0]を変数fileへ取得
+//     var file = $("#imageSelect").prop("files")[0];
+ 
+//     var fr = new FileReader();
+//     fr.onload = function() {
+//             //選択した画像を一旦imgタグに表示
+//             $("#preview").attr('src', fr.result);
+                        
+//             //imgタグに表示した画像をimageオブジェクトとして取得
+//             var image = new Image();
+//             image.src = $("#preview").attr('src');
+                        
+//             //縦横比を維持した縮小サイズを取得
+//             var w = 600;
+//             var ratio = w / image.width;
+//             var h = image.height * ratio;
+                        
+//             //canvasに描画
+//             var canvas = $("#canvas");
+//             var ctx = canvas[0].getContext('2d');
+//             $("#canvas").attr("width", w);
+//             $("#canvas").attr("height", h);
+//             ctx.drawImage(image, 0, 0, w, h);      
+//         };
+
+//         fr.readAsDataURL(file);
+
+// </script>
