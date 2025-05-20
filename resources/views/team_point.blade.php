9a7ca4bc06c0d2f49023566acda3461c32136bfd
ashintai <shintai.akira@gmail.com>
2025-05-20 15:56:41 +0900

    ｓたｆｆ３３ｓたｆｆ３３

--- resources/views/team_point.blade.php
+++ resources/views/team_point.blade.php
@@ -86,13 +86,16 @@
 <form class="ms-4" action="{{ route('team_point_input' , ['id' => $point->team_no, ] ) }}" method="POST" >
     @csrf
     <div class="form-group">
-        <label for="point_no">ポイント番号:</label>
-        <input type="text" id="point_no" name="point_no" required maxlength="4" inputmode="numeric" pattern="\d*">
-        <button type="button" id = "button_input" class="btn btn-primary">入力</button>
+        <div>
+            <label for="point_no">ポイント番号:</label>
+            <input type="text" id="point_no" name="point_no" required maxlength="4" inputmode="numeric" pattern="\d*">
+            <button type="button" id = "button_input" class="btn btn-primary">入力</button>
+        </div>
         <div>
             <label for="point_name_disp" class="align-middle">ポイント名:</label>
             <p id="point_name_disp" class="align-middle">  ポイント名 </p>
             <button type="submit" id = "button_submit" class="btn btn-success">登録</button>
+        <p class="align-middle">手入力のポイント番号は、OKとして登録されます。</p>
         </div>
     </div>
 </form>
