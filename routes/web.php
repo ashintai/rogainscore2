2c091be08e332d5cf1ba497429e066d1f7c7ff60
ashintai <shintai.akira@gmail.com>
2025-05-20 14:22:56 +0900

    staff16

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::get('/team_point_photo/{id}' , [HomeController::class , 'team_point_photo
 // ポイント編集画面から削除
 Route::post('/team_point_delete' , [HomeController::class , 'team_point_delete'])->name('team_point_delete');
 // ポイント編集画面から未確認へ
-Route::post('/team_point_change_mikaku' , [HomeController::class , 'team_point_ng'])->name('team_point_ng');
+Route::post('/team_point_change_mikaku' , [HomeController::class , 'team_point_mikaku'])->name('team_point_mikaku');
 // ポイント編集画面からOKへ
 Route::post('/team_point_change_ok' , [HomeController::class , 'team_point_ok'])->name('team_point_ok');
 // ポイント編集画面からNGへ
