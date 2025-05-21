396d02b1bf2139b3e36412c96ed8c832dce3e98b
ashintai <shintai.akira@gmail.com>
2025-05-21 10:11:10 +0900

    staff03

--- resources/views/team_point.blade.php
+++ resources/views/team_point.blade.php
@@ -35,7 +35,7 @@
 @foreach ($get_points as $point)
     @if ($point->setPoint && $point->setPoint->point_name)
     <div class="display: flex;" >
-    <div class="d-flex align-items-left " style="gap: 8px; margin-bottom: 0;">
+    
     <!-- 状態を表示 -->
     <!-- $point->checked 0 未確認 1確認中 2OK 3NG 4仮登録 5手入力＝OK -->
         @if($point->checked == 0)
@@ -53,10 +53,8 @@
         @endif
     <!-- ポイント番号とポイント名を表示 -->
         <h6 style="margin-left: 8px; margin-bottom: 0;">{{ $point->point_no }}:{{ $point->setPoint->point_name }}</h6>
-    </div>
     
-        <!-- 編集ボタンを表示 -->
-    <div class="d-flex justify-content-end " style="gap: 8px;">
+
     <!-- 写真 または　削除　-->
     <!-- cheked=5 の手入力の場合は　削除、写真がある場合は　写真を表示 -->
         @if($point->checked == 5)
@@ -79,8 +77,7 @@
         @elseif($point->checked == 4)
             <p>ポイント番号不明のため編集不可</p>
         @endif -->
-    </div> 
-    </div>
+   
     @endif
     <hr>
 @endforeach
