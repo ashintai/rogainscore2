ce82f02a309fc4f9cae2741be31840e6c3ab929f
ashintai <shintai.akira@gmail.com>
2025-05-29 17:49:14 +0900

    check80

--- resources/views/team_point_change.blade.php
+++ resources/views/team_point_change.blade.php
@@ -132,7 +132,7 @@
     </div>
 
     <hr>
-        <form action="{{ route('team_point' , ['id' => $user_id] ) }}" method="POST" style="text-align: center;">
+        <form action="{{ route('team_point' , ['id' => $user_id] ) }}" method="GET" style="text-align: center;">
         @csrf
         <input type="hidden" name="user_id" value="{{ $user_id }}">
         <input type="hidden" name="get_id" value="{{ $get_id }}">
